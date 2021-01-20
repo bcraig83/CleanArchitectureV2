9f31b45880b960e9ac0efb760671e55489d92dda
Ben Craig <ben.craig@hse.ie>
2021-01-20 14:30:40 +0000

    Correct namespace.

--- test/integration/IntegrationTestDriverApi/Startup.cs
+++ test/integration/IntegrationTestDriverApi/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace IntegrationTestDriverApi
+namespace CleanArchitecture.IntegrationTestDriverApi
 {
     public class Startup
     {

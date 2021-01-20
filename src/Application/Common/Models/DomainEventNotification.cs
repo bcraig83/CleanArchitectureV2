c78fce96c1b4e397477f91c90816456827037ba0
Ben Craig <ben.craig@hse.ie>
2021-01-20 12:59:18 +0000

    Add domain event notification.

---
+++ src/Application/Common/Models/DomainEventNotification.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitecture.Domain.Common.Events;
+using MediatR;
+
+namespace CleanArchitecture.Application.Common.Models
+{
+    public class DomainEventNotification<TDomainEvent> : INotification
+        where TDomainEvent : DomainEvent
+    {
+        public DomainEventNotification(TDomainEvent domainEvent)
+        {
+            DomainEvent = domainEvent;
+        }
+
+        public TDomainEvent DomainEvent { get; }
+    }
+}

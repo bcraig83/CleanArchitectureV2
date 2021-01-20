a02d8411d3621488594075d6bd5a8cf8fcc40ba1
Ben Craig <ben.craig@hse.ie>
2021-01-20 12:34:24 +0000

    Fill out mapping test.

--- test/unit/Application.UnitTests/Common/Mappings/MappingTests.cs
+++ test/unit/Application.UnitTests/Common/Mappings/MappingTests.cs
@@ -1,6 +1,28 @@
-﻿namespace Application.UnitTests.Common.Mappings
+﻿using AutoMapper;
+using CleanArchitecture.Application.Common.Mappings;
+using Xunit;
+
+namespace Application.UnitTests.Common.Mappings
 {
     public class MappingTests
     {
+        private readonly IConfigurationProvider _configuration;
+        private readonly IMapper _mapper;
+
+        public MappingTests()
+        {
+            _configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            _mapper = _configuration.CreateMapper();
+        }
+
+        [Fact]
+        public void ShouldHaveValidConfiguration()
+        {
+            _configuration.AssertConfigurationIsValid();
+        }
     }
 }

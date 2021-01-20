7c0451c7edca2f6b508f093fb801ff44c8302b0d
Ben Craig <ben.craig@hse.ie>
2021-01-20 14:49:24 +0000

    Put an example stub in, to show how it would work.

---
+++ test/integration/Application.IntegrationTests/Fakes/EmailSenderStub.cs
@@ -0,0 +1,38 @@
+﻿using CleanArchitecture.Application.Common.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.IntegrationTests.Fakes
+{
+    public class EmailSenderStub : IEmailSender
+    {
+        public IList<EmailDetails> _recordedEmails { get; private set; } = new List<EmailDetails>();
+
+        public Task SendEmailAsync(string to, string from, string subject, string body)
+        {
+            _recordedEmails.Add(new EmailDetails(to, from, subject, body));
+            return Task.CompletedTask;
+        }
+
+        public void Clear()
+        {
+            _recordedEmails.Clear();
+        }
+    }
+
+    public class EmailDetails
+    {
+        public EmailDetails(string to, string from, string subject, string body)
+        {
+            To = to;
+            From = from;
+            Subject = subject;
+            Body = body;
+        }
+
+        public string To { get; private set; }
+        public string From { get; private set; }
+        public string Subject { get; private set; }
+        public string Body { get; private set; }
+    }
+}

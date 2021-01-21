36e54d777d2a8a8e96766b352b2d73912dc223b6
Ben Craig <ben.craig@hse.ie>
2021-01-21 15:34:57 +0000

    Add sandbox console app for quick testing.

---
+++ test/exploratory/Sandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Use this for quick tests...");
+        }
+    }
+}

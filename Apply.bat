b7d6a79e94ab2cc625e1a5b66114e7999dddacbf
Ben Craig <ben.craig@hse.ie>
2021-01-25 17:14:06 +0000

    Helper batch files for bringing up and down K8S services and deployments.

---
+++ Apply.bat
@@ -0,0 +1,6 @@
+kubectl apply -f .\rabbit-deployment.yaml
+kubectl apply -f .\rabbit-service.yaml
+kubectl apply -f .\sandboxapi-deployment.yaml
+kubectl apply -f .\sandboxapi-service.yaml
+kubectl apply -f .\webui-deployment.yaml
+kubectl apply -f .\webui-service.yaml
---
+++ Delete.bat
@@ -0,0 +1,6 @@
+kubectl delete -f .\rabbit-deployment.yaml
+kubectl delete -f .\rabbit-service.yaml
+kubectl delete -f .\sandboxapi-deployment.yaml
+kubectl delete -f .\sandboxapi-service.yaml
+kubectl delete -f .\webui-deployment.yaml
+kubectl delete -f .\webui-service.yaml
